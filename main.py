c8c2a4af9a22fdb099420ef36191f7a1a0f20771
Rusl4n4ik <russharipov5328059@gmail.com>
2022-02-10 15:47:35 +0500

    almost done

--- main.py
+++ main.py
@@ -63,12 +63,8 @@
 #                     admins['login'].append(login)
 # print(users)
 # print(admins)
-users = {'Ruslan4ik' : {
-    'password': '123',
-    'money': '1000'
-},'Yusuf' : {
-'password': '111',
-'money': '2000'}}
+users = {'Ruslan4ik': {'password': 123, 'money': 1000},
+         'Yusuf': {'password': 111, 'money': 2000}}
 #--------------------------
 admins = {'login': ['Ruslan4ik']}
 print("Добро пожаловать!")
@@ -87,23 +83,31 @@ if a == 1:
             print(users[login]['money'])
         if operations == 2:
             take = int(input("Какую сумму хотите снять со счета?: "))
-            users[login]['money'] = money - take
+            users[login]['money'] = users[login]['money'] - take
         if operations == 3:
             put = int(input("Какую сумму хотите положить на счет?: "))
-            users[login]['money'] = money + put
+            users[login]['money'] = users[login]['money'] + put
         if operations == 4:
             admin_become = int(input("Хотите стать админом?\n"))
             if admin_become == 1:
                 admin_password = input("Введите пароль для становления Admin: ")
                 if admin_password == '777':
                     print("Вы стали админом")
+                    print('------------')
+                    print('')
+                    print("Список всех пользователей: ")
                     admins['login'].append(login)
+                    all_users = [x for x in users]
+                    print(*all_users, sep='\n')
+                    print('')
+                    print('-------------')
         if operations == 5:
             login_n = input("Введите новый логин: ")
             password_n = input("Введите новый пароль: ")
-            users[login] = login_n
-            users[login]['password'] = password_n
+            users[login].update(password=password_n)
             users[login]['money'] = money
+            users[login_n] = users[login]
+            del users[login]
             print(users)
 if a == 2:
     login = input("Введите логин: ")
@@ -117,19 +121,22 @@ if a == 2:
         password = input("Введите верный пароль: ")
     if login in admins['login']:
         print("Вы вошли как админ")
+        all_users = [x for x in users]
+        print(all_users)
     else:
         print("Вы вошли как обычный пользователь")
     while True:
-        print("1.Баланс\n", "2.Снятие денег со счета\n", "3.Пополнить баланс\n", "4.Получить права админа\n","5.Замена логина и пароля\n", sep="")
+        print("1.Баланс\n", "2.Снятие денег со счета\n", "3.Пополнить баланс\n", "4.Получить права админа\n",
+              "5.Замена логина и пароля\n", sep="")
         operations = int(input("Какую операцию хотить выбрать?: "))
         if operations == 1:
             print(users[login]['money'])
         if operations == 2:
-            take1 = int(input("Какую сумму хотите снять со счета?: "))
-            users[login]['money'] = money - take1
+            take = int(input("Какую сумму хотите снять со счета?: "))
+            users[login]['money'] = users[login]['money'] - take
         if operations == 3:
-            put1 = int(input("Какую сумму хотите положить на счет?: "))
-            users[login]['money'] = money + put1
+            put = int(input("Какую сумму хотите положить на счет?: "))
+            users[login]['money'] = users[login]['money'] + put
         if operations == 4:
             admin_become = int(input("Хотите стать админом?\n"))
             if admin_become == 1:
@@ -137,12 +144,15 @@ if a == 2:
                 if admin_password == '777':
                     print("Вы стали админом")
                     admins['login'].append(login)
+                    all_users = [x for x in users]
+                    print(all_users)
         if operations == 5:
             login_n = input("Введите новый логин: ")
             password_n = input("Введите новый пароль: ")
-            users[login] = login_n
-            users[login]['password'] = password_n
-            users[login]['money'] =
+            users[login].update(password=password_n)
+            users[login]['money'] = money
+            users[login_n] = users[login]
+            del users[login]
             print(users)
 print(users)
 print(admins)
